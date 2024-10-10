0915f6447453b1a95733fc0e10248a0b7a411414
MathisB2 <mathis.bodin2@gmail.com>
2024-10-10 16:58:50 +0200

    question 3

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Get, Route, Tags } from "tsoa";
+import {Body, Controller, Get, Patch, Path, Post, Route, Tags} from "tsoa";
 import { GameDTO } from "../dto/game.dto";
 import { gameService } from "../services/game.service";
+import {ConsoleDTO} from "../dto/console.dto";
+import {consoleService} from "../services/console.service";
+import {notFound} from "../error/NotFoundError";
 
 @Route("games")
 @Tags("Games")
@@ -9,4 +12,35 @@ export class GameController extends Controller {
   public async getAllGames(): Promise<GameDTO[]> {
     return gameService.getAllGames();
   }
+
+
+  @Get("/{id}")
+  public async getGameById(@Path() id: number): Promise<GameDTO | null> {
+    const game = await gameService.getGame(id);
+    if (!game) notFound("Game");
+    return game;
+  }
+
+
+  // Crée un nouveau jeu
+  @Post("/")
+  public async createGame(
+      @Body() requestBody: GameDTO
+  ): Promise<GameDTO | null> {
+    const {title, console} = requestBody;
+    return gameService.createGame(title, console);
+  }
+
+
+  // Met à jour un jeu par ID
+  @Patch("{id}")
+  public async updateGame(
+      @Path() id: number,
+      @Body() requestBody: GameDTO
+  ): Promise<GameDTO | null> {
+    const { title , console} = requestBody;
+    return gameService.updateGame(id, title, console);
+  }
 }
+
+
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,6 +1,9 @@
 import { GameDTO } from "../dto/game.dto";
 import { Console } from "../models/console.model";
 import { Game } from "../models/game.model";
+import {ConsoleDTO} from "../dto/console.dto";
+import {ConsoleService} from "./console.service";
+import {notFound} from "../error/NotFoundError";
 
 export class GameService {
   public async getAllGames(): Promise<GameDTO[]> {
@@ -12,6 +15,39 @@ export class GameService {
         },
       ],
     });
+  }
+
+
+  public async getGame(id: number): Promise<GameDTO | null> {
+    return Game.findOne({
+      where:{
+        id:id
+      }
+    })
+  }
+
+  public async createGame(title: string, console: ConsoleDTO): Promise<GameDTO | null> {
+    const existingConsole = await Console.findByPk(console.id)
+    if(!existingConsole) return notFound("Console");
+
+    return Game.create({
+      title:title,
+      console_id:console.id,
+    });
+  }
+
+  public async updateGame(id: number, title: string, console: ConsoleDTO): Promise<GameDTO | null> {
+    const game = await Game.findByPk(id);
+    if (!game) return notFound("Game");
+
+    const existingConsole = await Console.findByPk(console.id);
+    if (!existingConsole) return notFound("Console");
+
+    game.title = title;
+    game.console_id = console.id;
+    await game.save();
+
+    return game;
   }
 }
 
